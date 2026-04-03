256c788bd93bc6e4f65bc898ce94bfcf4d7a4d15
Maridinesh S <Maridinesh2202@gmail.com>
2026-04-03 14:40:54 +0530

    Added IntermediatePractice file Loop

--- Looping Statement/ForLoop.py
+++ Looping Statement/ForLoop.py
@@ -1,4 +1,6 @@
 #Print numbers from 1 to 10
+from itertools import count
+
 for i in range(0,11):
     print(i)
 
@@ -31,6 +33,66 @@ for i in range(1,51):
 #Print numbers from 1 to 10, each on same line (use end=" ")
 for i in range(1,10):
     print(i , end=" ")
+
+#Find the sum of numbers from 1 to 10
+num = 0
+for i in range(1,11):
+    num += i
+    print(num)
+
+#Find the sum of even numbers from 1 to 20
+for i in range(1,20):
+    if i % 2 == 0:
+        i += i
+        print(i)
+
+#Find the sum of odd numbers from 1 to 20
+for i in range(1,20):
+    if i % 2 != 0:
+        i += i
+        print(i)
+
+#Count how many numbers are there from 1 to 100
+count = 0
+
+for i in range(1,101):
+    count += 1
+    print(count , end=", ")
+
+#Print multiplication table of any number (user input)
+num = int(input("Enter the number for multiplication table : "))
+for i in range(1,11):
+    print(num,"X", i ,"=" ,num * i)
+
+#Print multiplication table of 5
+for i in range(1,11):
+    print(5,"X", i ,"=" ,5 * i)
+
+'''*
+**
+***
+****
+*****'''
+for i in range(1,6):
+    print("*" *i)
+
+'''*****
+****
+***
+**
+*'''
+for i in range(5,0 ,-1):
+    print("*" *i)
+
+'''1
+12
+123
+1234
+12345'''
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+print()
 
 
 
